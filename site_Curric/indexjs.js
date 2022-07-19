1d3ceadbf776022bedd3435bf1e14a78409925ca
Dev Pai da Sabedoria <Andersondejesus525@gmail.com>
2022-07-19 09:49:53 -0300

    .

--- site_Curric/indexjs.js
+++ site_Curric/indexjs.js
@@ -1,10 +1,1 @@
-let nameShip = prompt("Qual é o nome da sua nave?")
-
-function speedUp(newVelocity) {
-	newVelocity = 5
-	return newVelocity
-}
-let option = prompt("escolha 1")
-if (option = "1") {
-	alert(newVelocity)
-}
+alert ("Elaborando...")
